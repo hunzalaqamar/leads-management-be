a0fe6cc3730012dd646297ac79a9cc4feaa216cf
hunzalaqamar <hunzla.qamar@gmail.com>
2025-04-13 15:04:56 +0500

    minor fixes

--- src/main/java/com/leadmanagement/lead_management_backend/LeadManagementBackendApplication.java
+++ src/main/java/com/leadmanagement/lead_management_backend/LeadManagementBackendApplication.java
@@ -1,6 +1,5 @@
 package com.leadmanagement.lead_management_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeadManagementBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(LeadManagementBackendApplication.class, args);
 	}
 
